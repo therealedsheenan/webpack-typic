f99f54f6fdd3932906d5f86d07ea72df15f8ea55
Sheenan Tenepre <sheenantenepre@gmail.com>
2016-12-27 18:37:37 +0900

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Webpack-typic
 
 A typical webpack configuration files for typical javascript project.
+
+
+### Run through:
+This repository is only for webpack purposes only.
+
+This is a configurable webpack setup in which you can use in your projects.
+
+It presumes that the user have already configured his/her project directory.
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,57 +1,57 @@
-import { resolve } from 'path';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import ProgressBarPlugin from 'progress-bar-webpack-plugin';
-import { getIfUtils, removeEmpty } from 'webpack-config-utils';
-import OfflinePlugin from 'offline-plugin';
-import InlineManifestWebpackPlugin from 'inline-manifest-webpack-plugin';
+import { resolve } from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import ProgressBarPlugin from 'progress-bar-webpack-plugin'
+import { getIfUtils, removeEmpty } from 'webpack-config-utils'
+import OfflinePlugin from 'offline-plugin'
+import InlineManifestWebpackPlugin from 'inline-manifest-webpack-plugin'
 
 const PATHS = {
-    app: resolve('src'),
-    output: resolve('public'),
-    styles: resolve('/src/client/styles/main.js'),
-};
+  app: resolve('src'),
+  output: resolve('public'),
+  entry: './client/index.js'
+}
 
 module.exports = env => {
-  const {ifProd, ifNotProd} = getIfUtils(env);
+  const {ifProd, ifNotProd} = getIfUtils(env)
   const config = {
+    context: resolve('src'),
     resolve: {
       modules: [
         resolve('./src/client'),
         'node_modules'
       ]
     },
-    context: PATHS.app,
-    entry: './client/browser.js',
+    entry: PATHS.entry,
     output: {
-        path: PATHS.output,
-        filename: ifProd('bundle.[name].[hash].js', 'bundle.[name].js'),
-        pathinfo: ifNotProd()
+      path: PATHS.output,
+      filename: ifProd('[name].[hash].js', '[name].js'),
+      pathinfo: ifNotProd(),
+      publicPath: '/public/'
     },
     devtool: ifProd('source-map', 'eval'),
     devServer: {
-        contentBase: PATHS.output,
-        hot: true,
-        inline: true,
+      contentBase: PATHS.output,
+      inline: true,
+      port: 8000,
+      historyApiFallback: true
     },
     module: {
       loaders: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: 'babel-loader'
+          loaders: ['babel'],
+          include: [
+            resolve('src')
+          ]
         },
-        // {
-        //     test: /\.scss$/,
-        //     exclude: /node_modules/,
-        //     loaders: ["style", "css?sourceMap", "sass?sourceMap"]
-        // },
         {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract({
             fallbackLoader: 'style',
-            loader: ['css', 'sass'],
+            loader: ['css', 'sass']
           })
         },
         {
@@ -59,42 +59,42 @@ module.exports = env => {
           loader: 'url-loader',
           query: {
             name: ifProd('[path][name].[ext]?[hash]', '[hash].[ext]'),
-            limit: 10000,
-          },
+            limit: 10000
+          }
         },
         {
           test: /\.(eot|ttf|wav|mp3)$/,
           loader: 'file-loader',
           query: {
-            name: ifProd('[path][name].[ext]?[hash]', '[hash].[ext]'),
-          },
-        },
+            name: ifProd('[path][name].[ext]?[hash]', '[hash].[ext]')
+          }
+        }
       ]
     },
     plugins: removeEmpty([
       new ProgressBarPlugin(),
       new webpack.HotModuleReplacementPlugin(),
       new webpack.NoErrorsPlugin(),
-      new ExtractTextPlugin(ifProd('styles.[name].[chunkhash].css', 'styles.[name].css')),
+      new ExtractTextPlugin(ifProd('styles.[name].css', 'styles.[name].[chunkhash].css')),
       ifProd(new InlineManifestWebpackPlugin()),
       ifProd(new webpack.optimize.CommonsChunkPlugin({
-          names: 'manifest',
+        names: ['vendor']
       })),
       new HtmlWebpackPlugin({
-          template: './index.html',
-          inject: 'body'
+        template: './index.html',
+        inject: 'body'
       }),
       new OfflinePlugin(),
       new webpack.DefinePlugin({
         'process.env': {
           NODE_ENV: ifProd('"production"', '"development"')
         }
-      }),
+      })
     ])
-  };
-  if ( env.debug ) {
-    console.log(config);
+  }
+  if (env.debug) {
+    console.log(config)
     debugger // eslint-disable-line
   }
-  return config;
-};
+  return config
+}
